3ee69bab6a61b45f29a9c8129c81e463bfe7a338
Uzair Ansari <uzairansaridev911@gmail.com>
2026-06-25 18:14:38 +0500

    update

--- Pages/Dashboard.jsx
+++ Pages/Dashboard.jsx
@@ -159,7 +159,7 @@ const Dashboard = () => {
 
     navigate('/Settings')
 
-  }
+  };
 
   const goToPeople = async () => {
 
@@ -167,7 +167,15 @@ const Dashboard = () => {
 
     navigate('/People')
 
-  }
+  };
+
+  const goToNotifications = async () =>{
+
+    await triggerExit();
+
+    navigate('/Notifications')
+
+  };
 
   return (
     <div className="flex min-h-screen bg-gray-50">
@@ -233,7 +241,7 @@ const Dashboard = () => {
             </div>
 
             <div className="flex items-center space-x-2 sm:space-x-4 shrink-0">
-              <button className="relative p-2 rounded-full hover:bg-gray-100 transition-all duration-200">
+              <button onClick={goToNotifications} className="relative hover:cursor-pointer p-2 rounded-full hover:bg-gray-100 transition-all duration-200">
                 <svg className="w-5 h-5 sm:w-6 sm:h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                 </svg>
